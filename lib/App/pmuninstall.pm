6367d9441f0ac42e149402342a2c3aa512b50c99
Yuji Shimada <xaicron@cpan.org>
2010-08-03 14:41:47 +0000

    Import of XAICRON/App-pmuninstall-0.03 from CPAN.
    gitpan-cpan-distribution: App-pmuninstall
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         XAICRON/App-pmuninstall-0.03.tar.gz
    gitpan-cpan-author:       XAICRON
    gitpan-cpan-maturity:     released

---
+++ lib/App/pmuninstall.pm
@@ -0,0 +1,33 @@
+package App::pmuninstall;
+use strict;
+use warnings;
+our $VERSION = "0.03";
+
+1;
+__END__
+
+=head1 NAME
+
+App::pmuninstall - Uninstall modules
+
+=head1 DESCRIPTION
+
+App::pmuninstall is Fast module uninstaller.
+delete files from B<.packlist>.
+
+=head1 AUTHOR
+
+Yuji Shimada
+Tatsuhiko Miyagawa
+
+=head1 SEE ALSO
+
+L<pm-uninstall>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
