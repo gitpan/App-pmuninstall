dafbaa5c21f7cfcfe80b09a2eeb2fb47c2342186
Yuji Shimada <xaicron@cpan.org>
2010-08-06 05:25:06 +0000

    Import of XAICRON/App-pmuninstall-0.04 from CPAN.
    gitpan-cpan-distribution: App-pmuninstall
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         XAICRON/App-pmuninstall-0.04.tar.gz
    gitpan-cpan-author:       XAICRON
    gitpan-cpan-maturity:     released

--- lib/App/pmuninstall.pm
+++ lib/App/pmuninstall.pm
@@ -1,7 +1,7 @@
 package App::pmuninstall;
 use strict;
 use warnings;
-our $VERSION = "0.03";
+our $VERSION = "0.04";
 
 1;
 __END__
@@ -15,9 +15,44 @@ App::pmuninstall - Uninstall modules
 App::pmuninstall is Fast module uninstaller.
 delete files from B<.packlist>.
 
+=head1 SYNOPSIS
+
+uninstall MODULE
+
+  $ pm-uninstall App::pmuninstall
+
+=head1 OPTIONS
+
+=over
+
+=item -f, --force
+
+Uninstalls without prompts
+
+  $ pm-uninstall -f App::pmuninstall
+
+=item -v, --verbose
+
+Turns on chatty output
+
+  $ pm-uninstall -v CGI
+
+=item -c, --checkdeps
+
+Check dependencies
+
+  $ pm-uninstall -c Plack
+
+=item -h, --help
+
+Show help message
+
+  $ pm-uninstall -h
+
 =head1 AUTHOR
 
 Yuji Shimada
+
 Tatsuhiko Miyagawa
 
 =head1 SEE ALSO
